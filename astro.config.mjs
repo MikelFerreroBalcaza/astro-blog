0f251a790368390f15b56334f7ac874feb6c8967
Mikel <ferrerobalcazamikel@gmail.com>
2024-01-14 20:13:46 +0100

    009 Add postsCollection

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,9 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config'
 
-import preact from "@astrojs/preact";
+import preact from '@astrojs/preact'
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://astro-blog-kel.netlify.app/',
-  integrations: [preact()]
-});
+	site: 'https://astro-blog-kel.netlify.app/',
+	integrations: [preact()],
+})
